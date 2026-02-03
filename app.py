9ffd569d0a3d0608c559f0ecc0bb7d91880d4e69
Jagadeesh Vempati <137616197+Jagadeeshvempati@users.noreply.github.com>
2026-02-04 00:37:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import re
 # Load Initial Issues
 # -----------------------------
 # Excel file must be in the same repo
-df = pd.read_excel("AllKeywords.xlsx")
+df = pd.read_excel("All Events for Keywords.xlsx")
+
 
 
 # Normalize column names
